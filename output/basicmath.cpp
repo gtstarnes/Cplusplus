36e031b1b230b2c7947d166d2ef5069f58d98b67
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-11-17 21:15:45 -0500

    added std::endl

--- output/basicmath.cpp
+++ output/basicmath.cpp
@@ -8,16 +8,19 @@ Instructions:
 
 int a, b;
 
-
+// this function returns the sums of inputs
 int sum(int a, int b) {
     return a + b;
+}
+int times(int a, int b) {
+    return a * b;
 }
 
 int main() {
     a = 3;
     b = 4;
 
-    std::cout << sum(a, b);
-
+    std::cout << sum(a, b) << std::endl; // prints sum to console
+    std::cout << times(a, b) << std::endl;
     return 0;
 }
