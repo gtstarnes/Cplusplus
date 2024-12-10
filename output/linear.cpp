82b4d8a8636bc4e3ffa2fd99862b766812258f80
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-09 22:56:03 -0500

    created linear search function

--- output/linear.cpp
+++ output/linear.cpp
@@ -1,5 +1,17 @@
 #include<iostream>
 
+int linear(std::string t, std::string arr[]) {
+    for(int i = 0; i <=  5; i++) {
+        if (t == arr[i]){
+            return 1;
+        };
+    };
+    return -1;
+}
+
 int main() {
-    
+    std::string strings[5] = {
+        "a","b","c","d","e",
+    };
+    linear("f", strings);
 }
